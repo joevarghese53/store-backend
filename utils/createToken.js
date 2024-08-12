7b4cae588f121bd00446b86a7467bdf6e84414b8
Joe Varghese <joevarghese53@gmail.com>
2024-08-12 14:20:41 +0530

    samesite

--- utils/createToken.js
+++ utils/createToken.js
@@ -6,10 +6,10 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
-  res.cookie("jwt", token, {
+  res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === 'production', // Use `true` in production
+    sameSite: 'None', // Allow cross-origin cookies
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
