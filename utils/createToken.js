caea16b6633de0c993a4f655207d9c4a3f074447
Joe Varghese <joevarghese53@gmail.com>
2024-08-14 22:59:35 +0530

    cookie update

--- utils/createToken.js
+++ utils/createToken.js
@@ -8,10 +8,11 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // Use `true` in production
-    sameSite: 'None', // Allow cross-origin cookies
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
+    secure: process.env.NODE_ENV === 'production', // Ensure secure cookies in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Use None for cross-origin in production
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+});
+
 
   return token;
 };
