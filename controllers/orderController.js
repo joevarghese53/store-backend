d3dca88b7a26a7f7618fce2c89bd8ce28046af9a
Joe Varghese <joevarghese53@gmail.com>
2024-09-08 12:28:45 +0530

    user orders paid only

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -91,7 +91,7 @@ const getAllOrders = async (req, res) => {
 
 const getUserOrders = async (req, res) => {
   try {
-    const orders = await Order.find({ user: req.user._id });
+    const orders = await Order.find({ user: req.user._id, isPaid: true });
     res.json(orders);
   } catch (error) {
     res.status(500).json({ error: error.message });
