e564bf3ae4b531384ff4747b69e1ce01b0aaa540
Joe Varghese <joevarghese53@gmail.com>
2024-10-30 00:55:46 +0530

    partitioned: true,

--- utils/createToken.js
+++ utils/createToken.js
@@ -8,6 +8,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
+    partitioned: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
