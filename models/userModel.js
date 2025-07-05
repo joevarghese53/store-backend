a19b1011f4680c88b5d25b4da8df2b84f600b0a3
Joe Varghese <joevarghese53@gmail.com>
2025-07-05 18:29:27 +0530

    user model revert changes

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    mobile: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
     resetPasswordToken: { type: String },
